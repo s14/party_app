12ab377cbe66e3de5354f30ab412ab0c6821a6ca
Raghu Betina <git@rugb.com>
2014-08-13 12:25:41 -0500

    Added cookies

--- app/controllers/wristbands_controller.rb
+++ app/controllers/wristbands_controller.rb
@@ -3,6 +3,13 @@ class WristbandsController < ApplicationController
   end
 
   def create
+    session[:person] = params[:nickname]
+
+    if session[:person] == "Jack"
+      session[:wristband] = "red"
+    else
+      session[:wristband] = "green"
+    end
 
     redirect_to root_url
   end
